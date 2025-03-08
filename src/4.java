2fe0f1a0f81b0615fa3d0256670c5ac333f977d8
elanortuy2000 <elanortuy2000@gmx.com>
2025-03-08 08:11:32 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class MathClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("The sum is: " + (num1 + num2));
+    }
+}
